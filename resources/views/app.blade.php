686b43403e2cda53492fe46b293c768199d31975
julianasued <julianasuedprof@gmail.com>
2024-09-19 13:40:52 -0300

    feat: adiciona bootstrap no projeto

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html dir="ltr">
+@include('includes.head')
+<body>
+<div class="container">
+        <h1 class="text-center mt-5">Testando o Bootstrap!</h1>
+        <p class="text-center">Bootstrap com Laravel.</p>
+        
+        <div class="text-center">
+            <button class="btn btn-primary">Botão de Teste</button>
+        </div>
+    </div>
+@include('includes.scripts')
+</body>
+</html>
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="RUTicket">
+    <meta name="author" content="RUTicket">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('/images/favicon.png') }}">
+
+    <title>RUTicket</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- @yield('style') -->
+</head>
