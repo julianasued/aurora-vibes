6d7d085acada5dc5d610b8ee300cf69859debba4
Renato <renatojrpsn@gmail.com>
2024-09-26 17:37:58 -0300

    alterações e criação da index aluno

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,12 @@ use App\Http\Controllers\Ticket;
 
 Route::get('/', function () {
     return view('app');
+
+});
+
+Route::get('/compras', function () {
+    return view('alunos.index');
+
 });
 
 Route::resource('tickets', Ticket::class);
