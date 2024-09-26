05b424a08e82faa88fcec3af8c72a609296e2019
julianasued <julianasuedprof@gmail.com>
2024-09-26 16:46:35 -0300

    fix/3

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+echo "Installing Node.js dependencies..."
+cd /var/www/html
+npm install
+
+echo "Building assets with Breeze..."
+
+npm run production
+
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
@@ -10,14 +18,6 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force 
-
-echo "Installing Node.js dependencies..."
-cd /var/www/html
-npm install
-
-echo "Building assets with Breeze..."
-
-npm run production
 
 echo "Publishing cloudinary provider..."
 php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
