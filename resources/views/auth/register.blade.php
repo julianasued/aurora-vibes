6d6f79a1f6ff5c861462d42acdc4125a4645c516
filipejml <brincandodedesign@gmail.com>
2024-10-10 14:13:32 -0300

    feat: add role do login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,15 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <div class="mt-4">
+            <label for="role">Role</label>
+            <select name="role" id="role" class="form-control">
+                <option value="admin">Admin</option>
+                <option value="pad">Pad</option>
+                <option value="aluno" selected>Aluno</option>
+            </select>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
